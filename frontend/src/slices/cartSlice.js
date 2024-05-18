b70c66eb3888ea1462e9dac6435846995e9f1e3a
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-05-18 12:06:56 +0530

    cartslice actions

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,10 +4,44 @@ const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
   : { cartItems: [] };
 
+const addDecimal = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {},
-});
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
 
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      //calculate items price
+      state.itemsPrice = addDecimal(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+      //cal shipping price
+      state.shippingPrice = addDecimal(state.itemsPrice > 100 ? 0 : 10);
+
+      //cal tax price
+      state.taxprice = addDecimal(Number((0.15 * state.itemsPrice).toFixed(2)));
+
+      //cal total price
+
+      state.totalPrice =
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxprice).toFixed(2);
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
+  },
+});
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
