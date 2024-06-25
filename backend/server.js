23313e06151eb240f56ef1d6a737032523f3a73b
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-06-25 21:05:37 +0530

    paypal route

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
 import express from 'express';
 import cors from 'cors';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
+
 import connectDB from './config/db.js';
 import productsRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import cookieParser from 'cookie-parser';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import { config } from './config/config.js';
 
 const app = express();
 
 //Body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
 app.use(cookieParser());
 
 app.use(cors());
 
-const PORT = 5000;
+const port = config.port || 4000;
+
 connectDB();
 
 app.get('/', (req, res) => {
@@ -28,7 +33,14 @@ app.use('/api/products', productsRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
+app.get(
+  '/api/config/paypal',
+  (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+
+  //client is not there
+);
+
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
+app.listen(port, () => console.log(`Server is running at ${port}`));
