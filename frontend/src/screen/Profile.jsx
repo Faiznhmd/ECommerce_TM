40b027033de7142d764fc394628dce080d5f4ba9
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-06-25 21:52:38 +0530

    update userfile

--- frontend/src/screen/Profile.jsx
+++ frontend/src/screen/Profile.jsx
@@ -1,5 +1,108 @@
+import React, { useEffect, useState } from 'react';
+import { Table, Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { FaTimes } from 'react-icons/fa';
+
+import { toast } from 'react-toastify';
+import Message from '../component/Message';
+import Loader from '../component/Loader';
+
+import { useProfileMutation } from '../slices/userapislice.js';
+import { setCredentials } from '../slices/authSlice.js';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const { userInfo } = useSelector((state) => state.auth);
+  const [updateProfile, { isLoading: loadingUpdateProfile }] =
+    useProfileMutation();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error('Password does not Match');
+    } else {
+      try {
+        const res = await updateProfile({
+          _id: userInfo._id,
+          name,
+          email,
+          password,
+          confirmPassword,
+        }).unwrap();
+        dispatch(setCredentials(res));
+        toast.success('Profile updated Successfully');
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      setName(userInfo.name);
+      setEmail(userInfo.email);
+    }
+  }, [userInfo, userInfo.name, userInfo.email]);
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name" className="my-2">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="email" className="my-2">
+            <Form.Label>E-mail Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter your E-mail Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="password" className="my-2">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter your Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword" className="my-2">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="confirmPassword"
+              placeholder="Enter your Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="primary" className="my-2">
+            Update
+          </Button>
+          {loadingUpdateProfile && <Loader />}
+        </Form>
+      </Col>
+      <Col md={9}></Col>
+    </Row>
+  );
 };
 
 export default Profile;
