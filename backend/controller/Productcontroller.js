f7c96c79ba6399707f0b1f6215f870aaf5318a7b
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-06-26 18:16:44 +0530

    product controller

--- backend/controller/Productcontroller.js
+++ backend/controller/Productcontroller.js
@@ -1,12 +1,14 @@
 import asyncHandler from '../middleware/asyncHandlers.js';
 import Product from '../models/productModel.js';
 
+//get product
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
 
   res.json({ data: products });
 });
 
+//get product by ID
 const getProductsById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
@@ -17,4 +19,45 @@ const getProductsById = asyncHandler(async (req, res) => {
   throw new Error('Resource Not Found');
 });
 
-export { getProducts, getProductsById };
+// create a product
+
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/phone.jpg',
+    brand: 'Smaple Brand',
+    category: 'Sample Category',
+    countInStock: 0,
+    numsReviews: 0,
+    description: 'Sample description',
+  });
+  const createProduct = await product.save();
+  res.status(201).json(createProduct);
+});
+
+//update a product
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Resource Not Found');
+  }
+});
+export { getProducts, getProductsById, createProduct, updateProduct };
