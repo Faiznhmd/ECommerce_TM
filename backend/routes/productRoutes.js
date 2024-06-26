45b78248fef24553231678d900c35f5fd5c18835
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-06-26 18:16:57 +0530

    product  routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 const router = express.Router();
+import { protect, admin } from '../middleware/authMiddleware.js';
 import {
+  createProduct,
   getProducts,
   getProductsById,
+  updateProduct,
 } from '../controller/Productcontroller.js';
 
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductsById);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id').get(getProductsById).put(protect, admin, updateProduct);
 
 export default router;
