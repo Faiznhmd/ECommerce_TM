65f953ca77b8329a33e1eb05625ff19aa8e29bba
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-06-27 16:18:52 +0530

    img upload route

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -6,6 +7,7 @@ import connectDB from './config/db.js';
 import productsRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import UploadRoutes from './routes/UplaodRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import { config } from './config/config.js';
 
@@ -32,6 +34,7 @@ app.get('/', (req, res) => {
 app.use('/api/products', productsRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', UploadRoutes);
 
 app.get(
   '/api/config/paypal',
@@ -39,6 +42,9 @@ app.get(
 
   //client is not there
 );
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
