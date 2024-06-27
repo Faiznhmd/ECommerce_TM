2ecb5a4d1c2e59e002d0c5fc59c9330469bac159
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-06-27 16:19:26 +0530

    delete product

--- backend/controller/Productcontroller.js
+++ backend/controller/Productcontroller.js
@@ -60,4 +60,23 @@ const updateProduct = asyncHandler(async (req, res) => {
     throw new Error('Resource Not Found');
   }
 });
-export { getProducts, getProductsById, createProduct, updateProduct };
+
+//DELETE  A PRODUCT
+
+const DeleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await Product.deleteOne({ _id: product._id });
+    res.status(200).json({ message: 'Product deleted' });
+  } else {
+    res.status(404);
+    throw new Error('Resource Not Found');
+  }
+});
+export {
+  getProducts,
+  getProductsById,
+  createProduct,
+  updateProduct,
+  DeleteProduct,
+};
