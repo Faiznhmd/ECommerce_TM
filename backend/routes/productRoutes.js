ebaef4cf22705295fafbb696cdfb8a0cf8041d22
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-06-28 16:34:05 +0530

    route top product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,14 +8,16 @@ import {
   updateProduct,
   DeleteProduct,
   createProductReviews,
+  getTopProducts,
 } from '../controller/Productcontroller.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.get('/top', getTopProducts);
 router
   .route('/:id')
   .get(getProductsById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, DeleteProduct);
-router.route(':/id/reviews').post(protect, createProduct);
+router.route(':/id/reviews').post(protect, createProductReviews);
 
 export default router;
