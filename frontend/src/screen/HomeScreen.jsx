c75e04ed09e6079c38313b4435da8f2173a20cc2
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-06-28 16:37:26 +0530

    paginate carausal added

--- frontend/src/screen/HomeScreen.jsx
+++ frontend/src/screen/HomeScreen.jsx
@@ -1,27 +1,47 @@
 import { Col, Row } from 'react-bootstrap';
 import Product from '../component/Product.jsx';
 import Loader from '../component/Loader.jsx';
+import Paginate from '../component/Paginate.jsx';
+import ProductCarausal from '../component/ProductCarausal.jsx';
 import { useGetProductsQuery } from '../slices/ProductAPIslice.js';
+import { Link, useParams } from 'react-router-dom';
+import Message from '../component/Message.jsx';
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber, keyword } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({
+    keyword,
+    pageNumber,
+  });
 
   return (
     <>
+      {!keyword ? (
+        <ProductCarausal />
+      ) : (
+        <Link to="/" className="btn btn-light">
+          Go Back
+        </Link>
+      )}
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <div> {error?.data?.message || error.error} </div>
+        <Message> {error?.data?.message || error.error} </Message>
       ) : (
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products?.data.map((product) => (
+            {data.products?.map((product) => (
               <Col key={product._id} lg={4} sm={12} md={6} xl={3}>
                 <Product product={product} />
               </Col>
             ))}
-          </Row>{' '}
+          </Row>
+          <Paginate
+            pages={data.pages}
+            page={data.page}
+            keyword={keyword ? keyword : ''}
+          />
         </>
       )}
     </>
